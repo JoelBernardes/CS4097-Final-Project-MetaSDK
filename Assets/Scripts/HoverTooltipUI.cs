fe3821208c0f52d63d610559836cbc1e036d1bf9
Dylan Dinio <DDinio2101@gmail.com>
2024-10-28 04:29:15 -0400

    finished UI hover tool tips and developed spawning system from box

--- Assets/Scripts/HoverTooltipUI.cs
+++ Assets/Scripts/HoverTooltipUI.cs
@@ -6,24 +6,33 @@ public class HoverTooltipUI : MonoBehaviour
 {
     public string tooltipName;
     public string tooltipDescription;
-    public GameObject tooltipPrefab; // Assign the Tooltip prefab here
-
+    public GameObject tooltipPrefab; 
+    public float xOffset = 0.5f;
+    public float yOffset = 0.5f;
     private GameObject tooltipInstance;
 
     void Update()
     {
+        // always make the menu face the player
+        if (tooltipInstance != null)
+        {
+            tooltipInstance.transform.LookAt(GameObject.FindGameObjectWithTag("MainCamera").transform.position);
+        }
     }
 
+    // spawns in the tooltip prefab UI at the set offset from the object
     public void ShowTooltip()
     {
-        tooltipInstance = Instantiate(tooltipPrefab, this.transform.position + new Vector3(1, 1, 0), this.transform.rotation);
+        tooltipInstance = Instantiate(tooltipPrefab, this.transform.position + new Vector3(xOffset, yOffset, 0), tooltipPrefab.transform.rotation);
         TooltipUI tooltipUI = tooltipInstance.GetComponent<TooltipUI>();
+        tooltipInstance.transform.LookAt(GameObject.FindGameObjectWithTag("MainCamera").transform.position);
         if (tooltipUI != null)
         {
             tooltipUI.SetTooltip(tooltipName, tooltipDescription);
         }
     }
 
+    // delete the tooltip from the game as to not take processing power
     public void HideTooltip()
     {
         if (tooltipInstance != null)
---
+++ Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// spawns a given list of objects passed in through the inspector randomly until everything has been spawned
+public class ItemSpawner : MonoBehaviour
+{
+    // List of GameObjects to spawn
+    public List<GameObject> objectsToSpawn;
+
+    // Flag to control spawning
+    private bool canSpawn = true;
+
+    // This method is called when another collider enters the trigger collider
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the other collider has a specific tag (e.g., "Player")
+        if (canSpawn)
+        {
+            StartCoroutine(SpawnObject());
+        }
+    }
+
+    private IEnumerator SpawnObject()
+    {
+        // Check if there are objects left to spawn
+        if (objectsToSpawn.Count > 0)
+        {
+            // Choose a random object from the list
+            int randomIndex = Random.Range(0, objectsToSpawn.Count);
+            GameObject objectToSpawn = objectsToSpawn[randomIndex];
+
+            // Instantiate the object at the spawner's position and rotation
+            Instantiate(objectToSpawn, transform.position, transform.rotation);
+
+            // Optionally remove the spawned object from the list
+            objectsToSpawn.RemoveAt(randomIndex);
+        }
+
+        // Set canSpawn to false and wait for 1 second
+        canSpawn = false;
+        yield return new WaitForSeconds(1f);
+        canSpawn = true;
+    }
+}
