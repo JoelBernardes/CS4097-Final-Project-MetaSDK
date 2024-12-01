3aad5941ec835025e3478a4964e5ee22484c6de6
Dylan Dinio <35245790+dkd2101@users.noreply.github.com>
2024-12-01 18:38:26 -0500

    made hovertips appear on hover and click. Adjusted prefabs to accmodate new ui flow. (#11)

--- Assets/Scripts/HoverTooltipUI.cs
+++ Assets/Scripts/HoverTooltipUI.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Meta.XR.ImmersiveDebugger.UserInterface.Generic;
 using UnityEngine;
 
 public class HoverTooltipUI : MonoBehaviour
 {
     public string tooltipName;
     public string tooltipDescription;
-    public GameObject tooltipPrefab; 
+    public GameObject tooltipPrefab;
     public float xOffset = 0.5f;
     public float yOffset = 0.5f;
+
+    public OVRInput.Controller controller = OVRInput.Controller.RTouch;
+    public OVRInput.Button button = OVRInput.Button.One;
     private GameObject tooltipInstance;
+
+    private bool isHovered = false;
+    private bool toggled = false;
+    private bool isCooldown = false;
 
     void Update()
     {
@@ -17,13 +25,34 @@ public class HoverTooltipUI : MonoBehaviour
         if (tooltipInstance != null)
         {
             tooltipInstance.transform.LookAt(GameObject.FindGameObjectWithTag("MainCamera").transform.position);
+            tooltipInstance.transform.position = Vector3.MoveTowards(tooltipInstance.transform.position, this.transform.position + new Vector3(xOffset, yOffset, 0), 1.0f);
         }
+
+        if (OVRInput.Get(button, controller) && !isCooldown)
+        {
+            if (!toggled)
+            {
+                ShowTooltip();
+                toggled = true;
+            }
+            else
+            {
+                HideTooltip();
+                toggled = false;
+            }
+            StartCoroutine(ToggleCooldown());
+        }
+    }
+
+    public void SetHovering(bool hovering)
+    {
+        isHovered = hovering;
     }
 
     // spawns in the tooltip prefab UI at the set offset from the object
     public void ShowTooltip()
     {
-        if (tooltipInstance != null)
+        if (tooltipInstance != null || !isHovered)
         {
             return;
         }
@@ -40,10 +69,17 @@ public class HoverTooltipUI : MonoBehaviour
     // delete the tooltip from the game as to not take processing power
     public void HideTooltip()
     {
-        if (tooltipInstance != null)
+        if (tooltipInstance != null && isHovered)
         {
             Destroy(tooltipInstance);
             tooltipInstance = null;
         }
+    }
+
+    private IEnumerator ToggleCooldown()
+    {
+        isCooldown = true;  // Start cooldown
+        yield return new WaitForSeconds(0.5f);  // Wait for the set delay time
+        isCooldown = false;  // End cooldown
     }
 }
